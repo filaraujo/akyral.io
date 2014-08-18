4187cd024e088704587c7937d11f4e59a456268e
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-08-17 23:08:49 -0400

    updating css, structure and dependencies

--- src/assets/scripts/akyral.js
+++ src/assets/scripts/akyral.js
@@ -1,7 +1,23 @@
-(function(){
+(function() {
     var menu = document.querySelector('button[name="menu"]');
 
-    menu.addEventListener('click', function(){
+    menu.addEventListener('click', function() {
         document.querySelector('akyral-layout').toggleDrawer();
     });
 }());
+
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-53633088-1', 'auto');
+ga('send', 'pageview');
