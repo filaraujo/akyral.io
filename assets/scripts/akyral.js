30b8583b1ca001ba854e83f174a45e6636e7d0d6
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-09-03 21:00:27 -0400

    Update 2014-09-04T01:00:27.680Z

--- assets/scripts/akyral.js
+++ assets/scripts/akyral.js
@@ -20,4 +20,5 @@
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-53633088-1', 'auto');
+ga('require', 'displayfeatures');
 ga('send', 'pageview');
