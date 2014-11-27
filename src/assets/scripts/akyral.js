38530f6e49e94bd780a2d074ed83152d9b872cf0
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-11-26 21:48:59 -0500

    more cleanup

--- src/assets/scripts/akyral.js
+++ src/assets/scripts/akyral.js
@@ -1,22 +1,22 @@
 (function() {
-    var menu = document.querySelector('button[name="menu"]');
+  var menu = document.querySelector('button[name="menu"]');
 
-    menu.addEventListener('click', function() {
-        document.querySelector('akyral-layout').toggleDrawer();
-    });
+  menu.addEventListener('click', function() {
+    document.querySelector('akyral-layout').toggleDrawer();
+  });
 }());
 
 
 (function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date();
-    a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m)
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
 })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-53633088-1', 'auto');
