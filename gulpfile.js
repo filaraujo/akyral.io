431a09bdc636754bd6a55de8d8fb91df61cae3d5
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-11-26 21:58:14 -0500

    code style cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@ var clean = require('gulp-clean');
 var watch = require('gulp-watch');
 var webserver = require('gulp-webserver');
 var exec = require('child_process').exec;
-var deploy = require("gulp-gh-pages");
+var deploy = require('gulp-gh-pages');
 
 
 var paths = {
-    distDir: './dist',
-    src: './src/**/*',
-    components: [
-        './src/assets/components/**/*',
-        '!./src/assets/components/{akyral-app,akyral-app/**}',
-        '!./src/assets/components/{akyral-docs,akyral-docs/**}'
-    ],
-    templates: './templates/**/*'
+  distDir: './dist',
+  src: './src/**/*',
+  components: [
+    './src/assets/components/**/*',
+    '!./src/assets/components/{akyral-app,akyral-app/**}',
+    '!./src/assets/components/{akyral-docs,akyral-docs/**}'
+  ],
+  templates: './templates/**/*'
 };
 
 gulp.task('webserver', function() {
@@ -22,16 +22,20 @@ gulp.task('webserver', function() {
     .pipe(webserver({
       livereload: false,
       directoryListing: {
-        enable:true,
+        enable: true,
         path: paths.distDir
       },
       open: true
     }));
 });
 
-gulp.task('clean', function () {
-    return gulp.src(paths.components,{read: false})
-        .pipe(clean({force: true}));
+gulp.task('clean', function() {
+  return gulp.src(paths.components, {
+    read: false
+  })
+    .pipe(clean({
+      force: true
+    }));
 });
 
 
@@ -41,20 +45,20 @@ gulp.task('watch', function() {
 });
 
 
-gulp.task('build', function(cb){
-    return exec('node index.js', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-     });
+gulp.task('build', function(cb) {
+  return exec('node index.js', function(err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
 });
 
-gulp.task('deploy', function () {
-    gulp.src('dist/**/*.*')
-        .pipe(deploy({
-            cacheDir: '.cache' // needed for some odd reason
-        }))
-        .pipe(gulp.dest('.tmp'));
+gulp.task('deploy', function() {
+  gulp.src('dist/**/*.*')
+    .pipe(deploy({
+      cacheDir: '.cache' // needed for some odd reason
+    }))
+    .pipe(gulp.dest('.tmp'));
 });
 
 gulp.task('server', ['webserver', 'watch']);
