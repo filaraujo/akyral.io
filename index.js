932166a98e13e511711a1d9d6180b9d50fe92413
Filipe Araujo <filipe.alves.araujo@gmail.com>
2014-11-26 21:46:03 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,73 +1,77 @@
-var metalsmith  = require('metalsmith');
-var assets      = require('metalsmith-assets');
-var autoprefixer= require('metalsmith-autoprefixer');
-var templates   = require('metalsmith-templates');
+var metalsmith = require('metalsmith');
+var assets = require('metalsmith-assets');
+var autoprefixer = require('metalsmith-autoprefixer');
+var templates = require('metalsmith-templates');
 var collections = require('metalsmith-collections');
-var ignore      = require('metalsmith-ignore');
-var permalinks  = require('metalsmith-permalinks');
-var sass        = require('metalsmith-sass');
-var swig        = require('swig');
+var ignore = require('metalsmith-ignore');
+var permalinks = require('metalsmith-permalinks');
+var sass = require('metalsmith-sass');
+var swig = require('swig');
 
 
-function renderContents(config){
-    return function(files, metalsmith, done){
-        Object.keys(files).forEach(function(key){
-            var file = files[key];
+function renderContents(config) {
+  return function(files, metalsmith, done) {
+    Object.keys(files).forEach(function(key) {
+      var file = files[key];
 
-            if(file.component){
-                file.contents = new Buffer(swig.render(file.contents.toString(), { locals: file }));
-            }
-        });
-        done();
-    }
+      if (file.component) {
+        file.contents = new Buffer(swig.render(file.contents.toString(), {
+          locals: file
+        }));
+      }
+    });
+    done();
+  }
 }
 
-function setAssetPath(config){
-    return function(files, metalsmith, done){
-        Object.keys(files).forEach(function(key){
-            var file = files[key];
-            file.pathRoot = !!file.component ? '../' : '';
-        });
-        done();
-    }
+function setAssetPath(config) {
+  return function(files, metalsmith, done) {
+    Object.keys(files).forEach(function(key) {
+      var file = files[key];
+      file.pathRoot = !!file.component ? '../' : '';
+    });
+    done();
+  }
 }
 
 metalsmith(__dirname)
-    .use(setAssetPath())
-    .use(ignore([
-      'assets/components',
-      'assets/components/**/*',
-      'assets/components/**/.bower.json'
-    ]))
-    .use(collections({
-        components: {
-            pattern: 'components/*.html'
-        }
-    }))
-    .use(permalinks({
-        pattern: ':page'
-    }))
-    .use(sass({
-        outputDir: 'assets/styles',
-        outputStyle: 'compressed'
-    }))
-    .use(assets({
-        source: './src/assets/components',
-        destination: './assets/components'
-    }))
-    .use(renderContents())
-    .use(templates({
-        engine: 'swig',
-        partials: {
-            drawer: 'partials/drawer',
-            footer: 'partials/footer',
-            install: 'partials/install',
-            header: 'partials/header',
-            meta: 'partials/meta'
-        }
-    }))
-    .use(autoprefixer())
-    .destination('dist')
-    .build(function(err, files) {
-        if (err) { throw err; }
-    });
+  .use(setAssetPath())
+  .use(ignore([
+    'assets/components',
+    'assets/components/**/*',
+    'assets/components/**/.bower.json'
+  ]))
+  .use(collections({
+    components: {
+      pattern: 'components/*.html'
+    }
+  }))
+  .use(permalinks({
+    pattern: ':page'
+  }))
+  .use(sass({
+    outputDir: 'assets/styles',
+    outputStyle: 'compressed'
+  }))
+  .use(assets({
+    source: './src/assets/components',
+    destination: './assets/components'
+  }))
+  .use(renderContents())
+  .use(templates({
+    engine: 'swig',
+    partials: {
+      drawer: 'partials/drawer',
+      footer: 'partials/footer',
+      install: 'partials/install',
+      header: 'partials/header',
+      meta: 'partials/meta'
+    }
+  }))
+  .use(autoprefixer())
+  .destination('dist')
+  .build(function(err, files) {
+    if (err) {
+      throw err;
+    }
+  });
